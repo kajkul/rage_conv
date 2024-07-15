b313228ba3b5df56adf0e495dc7f4359616a2a51
kajkul <kajkul@o2.pl>
2024-07-15 20:41:46 +0200

    tweak: split unsafe calls

--- src/cw_processing/export.rs
+++ src/cw_processing/export.rs
@@ -3,34 +3,32 @@ use std::{ffi::CString, path::PathBuf, time::Instant};
 use crate::cw_processing::ffi;
 
 pub async fn texture_dict(file_path: PathBuf) {
-    unsafe {
-        let now = Instant::now();
-        println!(
-            "processing file: {:?}",
-            file_path.file_name().expect("no file name")
-        );
+    let now = Instant::now();
+    println!(
+        "processing file: {:?}",
+        file_path.file_name().expect("no file name")
+    );
 
-        let path = file_path.clone();
+    let path = file_path.clone();
 
-        let file_name = file_path
-            .file_name()
-            .expect("invalid filename")
+    let file_name = file_path
+        .file_name()
+        .expect("invalid filename")
+        .to_str()
+        .expect("invalid filename");
+
+    tokio::task::spawn_blocking(move || {
+        let path = path
             .to_str()
-            .expect("invalid filename");
+            .expect("cant convert path to string")
+            .to_owned();
 
-        tokio::task::spawn_blocking(move || {
-            let path = path
-                .to_str()
-                .expect("cant convert path to string")
-                .to_owned();
+        let c_path = CString::new(path).expect("cstring creation failed");
 
-            let c_path = CString::new(path).expect("cstring creation failed");
+        unsafe { ffi::cw_export_texture_dict(c_path.as_ptr() as *const u8) };
+    })
+    .await
+    .expect("spawing processing task failed");
 
-            return ffi::cw_export_texture_dict(c_path.as_ptr() as *const u8);
-        })
-        .await
-        .expect("spawing processing task failed");
-
-        println!("ytd export {} took: {:.2?}", file_name, now.elapsed());
-    }
+    println!("ytd export {} took: {:.2?}", file_name, now.elapsed());
 }
--- src/cw_processing/import.rs
+++ src/cw_processing/import.rs
@@ -11,66 +11,64 @@ use tokio::fs;
 use crate::{args::ARGS, cw_processing::ffi, libc_freer::LibcFreer};
 
 pub async fn xml(file_path: PathBuf) {
-    unsafe {
-        let now = Instant::now();
-        println!(
-            "processing file: {:?}",
-            file_path.file_name().expect("no file name")
-        );
+    let now = Instant::now();
+    println!(
+        "processing file: {:?}",
+        file_path.file_name().expect("no file name")
+    );
 
-        let res = tokio::task::spawn_blocking(move || {
-            let path = file_path
-                .to_str()
-                .expect("cant convert path to string")
-                .to_owned();
+    let res = tokio::task::spawn_blocking(move || {
+        let path = file_path
+            .to_str()
+            .expect("cant convert path to string")
+            .to_owned();
 
-            let c_path = CString::new(path).expect("cstring creation failed");
+        let c_path = CString::new(path).expect("cstring creation failed");
 
-            return ffi::cw_import_xml(c_path.as_ptr() as *const u8);
-        })
-        .await
-        .expect("spawing processing task failed");
+        unsafe { ffi::cw_import_xml(c_path.as_ptr() as *const u8) }
+    })
+    .await
+    .expect("spawing processing task failed");
 
-        let mut out_path = PathBuf::new();
+    let mut out_path = PathBuf::new();
 
-        if res.data == null() || res.file_name == null() {
-            println!("null ptr from cw");
-            return;
+    if res.data == null() || res.file_name == null() {
+        println!("null ptr from cw");
+        return;
+    }
+
+    let slice = unsafe { std::slice::from_raw_parts(res.data, res.data_len) };
+    let data = unsafe { LibcFreer::new(slice, res.data as *mut c_void) };
+
+    if let Ok(file_name) = unsafe { CStr::from_ptr(res.file_name as *const i8).to_str() } {
+        let file_name = file_name.to_owned();
+        unsafe { libc::free(res.file_name as *mut libc::c_void) };
+
+        println!("import xml {} took: {:.2?}", file_name, now.elapsed());
+
+        let out_dir = ARGS
+            .get()
+            .expect("no args")
+            .output_dir
+            .to_str()
+            .expect("cant convert path to string");
+
+        out_path.push(out_dir);
+
+        out_path.push(file_name);
+
+        if !fs::try_exists(&out_dir)
+            .await
+            .expect("unable to check if dir exsists")
+        {
+            println!("creating output dir");
+            fs::create_dir_all(out_dir)
+                .await
+                .expect("unable to create output directory");
         }
 
-        let slice = std::slice::from_raw_parts(res.data, res.data_len);
-        let data = LibcFreer::new(slice, res.data as *mut c_void);
-
-        if let Ok(file_name) = CStr::from_ptr(res.file_name as *const i8).to_str() {
-            let file_name = file_name.to_owned();
-            libc::free(res.file_name as *mut libc::c_void);
-
-            println!("import xml {} took: {:.2?}", file_name, now.elapsed());
-
-            let out_dir = ARGS
-                .get()
-                .expect("no args")
-                .output_dir
-                .to_str()
-                .expect("cant convert path to string");
-
-            out_path.push(out_dir);
-
-            out_path.push(file_name);
-
-            if !fs::try_exists(&out_dir)
-                .await
-                .expect("unable to check if dir exsists")
-            {
-                println!("creating output dir");
-                fs::create_dir_all(out_dir)
-                    .await
-                    .expect("unable to create output directory");
-            }
-
-            fs::write(out_path, *data)
-                .await
-                .unwrap_or_else(|err| println!("error while writing res file {:?}", err));
-        }
+        fs::write(out_path, *data)
+            .await
+            .unwrap_or_else(|err| println!("error while writing res file {:?}", err));
     }
 }
